86ab74fb7476ae42dc6ab602f8c0e69aedf56cd7
bhupender <b.mehtaa2021@gmail.com>
2025-02-05 18:16:52 +0530

    typescript cohort code is pushed with comments

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+let x = 1; // this is how we define type is typescript without explicity defining the type code run karaha h coz typescript ne infere (assume) karliya ki 1 h toh number hi hoga but sometimes compiler is not able to infer a comples type tab u need to give a type expilcity
+// int i=1 ; this is how we define type in c++
+console.log(x);
+function greeting(firstName) {
+    console.log(`hello ${firstName}`);
+}
+greeting("anurag");
---
+++ index.ts
@@ -0,0 +1,13 @@
+let x: number = 1; // this is how we define type is typescript without explicity defining the type code run karaha h coz typescript ne infere (assume) karliya ki 1 h toh number hi hoga but sometimes compiler is not able to infer a comples type tab u need to give a type expilcity
+
+// int i=1 ; this is how we define type in c++
+
+console.log(x);
+
+function greeting(firstName: string) {
+  console.log(`hello ${firstName}`);
+}
+
+greeting("anurag");
+
+ // make a functiion that calculates the sum of two numver
